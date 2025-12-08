14e32ce708a6cd92a3b879145168dfd38791c064
Florent NASSCHAERT <92719458+Cronospy@users.noreply.github.com>
2025-12-08 11:18:15 +0100

    Add files via upload
    Fichiers nécessaires à la réalisation de la question 4

---
+++ un_autre_programme.c
@@ -0,0 +1,13 @@
+// Un commentaire
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]){
+    while(1){
+        //RIEN
+    }
+    return 0;
+}
---
+++ un_programme.c
@@ -0,0 +1,10 @@
+// Un commentaire
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]){
+    return 0;
+}
